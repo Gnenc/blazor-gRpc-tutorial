0a1c8306b4471d33ec54daaefbd24e4bbaacc393
Gonenc Filiz <gonenc@dakikyazilim.com>
2021-01-13 15:36:28 +0300

    Initial commit.

---
+++ Server/Services/GreeterService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcGreeter;
+
+public class GreeterService: Greeter.GreeterBase
+{
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        var reply = new HelloReply { Message = $"Hello from gRPC, {request.Name}!"};
+        return Task.FromResult(reply);
+    }
+}
